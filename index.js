896ce2e81f6af9909e53218417e62fda314dc999
Lauren Yu <laurenkyu@gmail.com>
2020-03-09 02:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ function wakeDog(dogName, dogBreed) {
   console.log(string);
   return string;
 }
+
+function leashDog(dogName, dogBreed) {
+  let string = `Leash ${dogName} the ${dogBreed}`;
+  console.log(string);
+  return string;
+}
+
+function walkToPark(dogName, dogBreed) {
+  let string = `Walk to the park with ${dogName} the ${dogBreed}`;
+  console.log(string);
+  return string;
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  let string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  console.log(string);
+  return string;
+}
+
+function walkHome(dogName, dogBreed) {
+  let string = `Walk home with ${dogName} the ${dogBreed}`;
+  console.log(string);
+  return string;
+}
+
+console.log("Walk home with Byron the poodle");
+console.log("Unleash Byron the poodle");
