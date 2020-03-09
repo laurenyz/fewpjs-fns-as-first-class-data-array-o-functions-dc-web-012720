3ef691c4b29e0e193b84bd4d19e09069b978770e
Lauren Yu <laurenkyu@gmail.com>
2020-03-09 02:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ function unleashDog(dogName, dogBreed) {
   return string;
 }
 
-let routine = [wakeDog(), leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()]
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
